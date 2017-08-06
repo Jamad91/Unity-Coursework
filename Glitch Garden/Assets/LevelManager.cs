eadcc1726c0b80a678ec3c9028f5632e06716c33
Jimmy <jdicolandrea@gmail.com>
2017-08-06 16:42:42 -0400

    add splash screen

---
+++ Glitch Garden/Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start ()
+	{
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log("Level Load Requested for: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame ()
+	{
+		Debug.Log("Qutting Game");
+		Application.Quit();
+	}
+
+}
